c8c4e8e437d6f8a4c1ac8f21e413331e87c33064
John Seekins <john@civiceagle.com>
2022-08-05 17:17:05 -0600

    condense routehandler package some more and document better
    Signed-off-by: John Seekins <john@civiceagle.com>

--- proxy/routehandler/routehandler.go
+++ proxy/routehandler/routehandler.go
@@ -2,15 +2,31 @@ package routehandler
 
 import (
 	"fmt"
+	"encoding/json"
+	"io"
 	"net/http"
+	"strings"
 
 	"github.com/civic-eagle/statsd-http-proxy/proxy/statsdclient"
+	log "github.com/sirupsen/logrus"
+	vmmetrics "github.com/VictoriaMetrics/metrics"
 )
+
+// 5 MB
+const maxBodySize = 5000 * 1024 * 1024
 
 // RouteHandler as a collection of route handlers
 type RouteHandler struct {
 	statsdClient statsdclient.StatsdClientInterface
 	metricPrefix string
+}
+
+// MetricRequest: internal representation of a metric to be written
+type MetricRequest struct {
+	Metric	string `json:"metric,omitempty"`
+	Value    int    `json:"value"`
+	Tags string `json:"tags"`
+	SampleRate float64 `json:"sampleRate"`
 }
 
 // NewRouteHandler creates collection of route handlers
@@ -26,7 +42,6 @@ func NewRouteHandler(
 
 	return &routeHandler
 }
-
 
 func (routeHandler *RouteHandler) HandleMetric(
 	w http.ResponseWriter,
@@ -75,4 +90,88 @@ func (routeHandler *RouteHandler) HandleMetricName(
 
 func (routeHandler *RouteHandler) HandleHeartbeatRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
+}
+
+func sendMetric(routeHandler *RouteHandler, metricType string, key string, value int, sampleRate float32) {
+	/*
+	Since we have two incoming handler paths for metrics
+	we need a common switch case to actually process each metric
+	once we've formatted it consistently
+	Simply actually increment the correct values in our internal
+	statsd client (and bump related internal metrics)
+	*/
+	switch metricType {
+	case "count":
+		routeHandler.statsdClient.Count(key, value, sampleRate)
+		vmmetrics.GetOrCreateCounter("counters_added_total").Inc()
+	case "gauge":
+		routeHandler.statsdClient.Gauge(key, value)
+		vmmetrics.GetOrCreateCounter("gauges_added_total").Inc()
+	case "timing":
+		routeHandler.statsdClient.Timing(key, int64(value), sampleRate)
+		vmmetrics.GetOrCreateCounter("timing_added_total").Inc()
+	case "set":
+		routeHandler.statsdClient.Set(key, value)
+		vmmetrics.GetOrCreateCounter("set_added_total").Inc()
+	}
+}
+
+func procBody(w http.ResponseWriter, r *http.Request) (MetricRequest, error) {
+	/*
+	All incoming POST requests to '/:type'
+	and '/:type/:metric' should have consistent
+	data (JSON object) that we can parse
+	*/
+	if contentType := r.Header.Get("Content-Type"); contentType != "application/json" {
+		http.Error(w, "Unsupported content type", 400)
+		return MetricRequest{}, fmt.Errorf("Unsupported content type")
+	}
+
+	body, err := io.ReadAll(io.LimitReader(r.Body, maxBodySize))
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return MetricRequest{}, err
+	}
+	r.Body.Close()
+
+	var req MetricRequest
+	if err := json.Unmarshal(body, &req); err != nil {
+		http.Error(w, err.Error(), 400)
+		return MetricRequest{}, err
+	}
+
+	return req, nil
+}
+
+func processTags(tagsList string) string {
+	/*
+	Process tags for a metric.
+	Starting with a string of comma-separated key=value pairs,
+	we need to ensure that:
+	1. each pair contains two items, one on each side of a '='
+	2. that both the key and the value strings have actual content ('=value' and 'key=' are not allowed)
+
+	Then return the resulting pairs to a string
+
+	Any failures means the metric gets no tags
+	*/
+	list := strings.Split(strings.TrimSpace(tagsList), ",")
+	if len(list) == 0 {
+		return ""
+	}
+
+	for _, pair := range list {
+		pairItems := strings.Split(pair, "=")
+		if len(pairItems) != 2 {
+			log.WithFields(log.Fields{"Tags": tagsList, "pair": pairItems}).Debug("Missing pair")
+			return ""
+		} else if len(strings.TrimSpace(pairItems[0])) == 0 {
+			log.WithFields(log.Fields{"Tags": tagsList, "pair": pairItems}).Debug("Invalid tag key")
+			return ""
+		} else if len(strings.TrimSpace(pairItems[1])) == 0 {
+			log.WithFields(log.Fields{"Tags": tagsList, "pair": pairItems}).Debug("Invalid tag value")
+			return ""
+		}
+	}
+	return "," + tagsList
 }

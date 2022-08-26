2c628845993adcdcf2a76fe0972956618ca1563a
John Seekins <john@civiceagle.com>
2022-08-26 08:44:33 -0600

    rename statsd client files
    Signed-off-by: John Seekins <john@civiceagle.com>

--- proxy/statsdclient/client.go
+++ proxy/statsdclient/client.go
@@ -1,5 +1,14 @@
 package statsdclient
 
+import GoMetricStatsdClient "github.com/GoMetric/go-statsd-client"
+
+func NewGoMetricClient(
+	statsdHost string,
+	statsdPort int,
+) StatsdClientInterface {
+	return GoMetricStatsdClient.NewClient(statsdHost, statsdPort)
+}
+
 type StatsdClientInterface interface {
 	Open()
 	Close()
